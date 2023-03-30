204173218f9df9f6eae74c46e299894fe5f34550
G00402357 <G00402357@atu.ie>
2023-03-30 13:15:20 +0100

    Basis Of Project, Old one was committed incorrectly. New one made.

---
+++ src/main/java/ie/atu/bankapp/Bank.java
@@ -0,0 +1,15 @@
+package ie.atu.bankapp;
+
+public class Bank {
+    //log in
+    //check account number
+    //check password
+    //take input
+    //database function to compare against
+
+
+
+    //create account
+    //take info
+    //store into database
+}
