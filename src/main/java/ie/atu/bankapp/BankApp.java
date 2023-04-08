f4b5cfa4cad674454bddd4805b09c6764c702f55
James Albright <G00379271@atu.ie>
2023-04-08 20:35:37 +0100

    Created delete function for tables to remove existing accounts.
    Created basic user interface to test delete function.

--- src/main/java/ie/atu/bankapp/BankApp.java
+++ src/main/java/ie/atu/bankapp/BankApp.java
@@ -1,6 +1,15 @@
 package ie.atu.bankapp;
 
-public class BankApp {
+import java.util.Scanner;
+
+public class BankApp extends BankDB{
     //face of the application
     //make it nice to interact with for the user
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.println("Enter account number: ");
+        Scanner sc = new Scanner(System.in);
+        int custNum = sc.nextInt();
+        Update(custNum);
+    }
 }
