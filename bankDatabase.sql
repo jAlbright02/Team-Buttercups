ad3999302bb5ea12fe31deed243538336ded9da3
jAlbright02 <114882053+jAlbright02@users.noreply.github.com>
2023-04-09 21:53:24 +0100

    Updated bankDB
    Changed the database in order to achieve exactly what's needed and nothing more.
    Removed unnecessary customer information, added user/pass varchars and changed balance to float

--- bankDatabase.sql
+++ bankDatabase.sql
@@ -1,33 +1,36 @@
-CREATE DATABASE bankDatabase;
+CREATE DATABASE bankDB;
 
-USE bankDatabase;
+USE bankDB;
+
+BEGIN;
 
 CREATE TABLE customers (
-	id INT NOT NULL AUTO_INCREMENT,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
+	id INT NOT NULL auto_increment,
+	name VARCHAR(50) NOT NULL,
+	userName VARCHAR(50) NOT NULL,
+	pass VARCHAR(20) NOT NULL,
 	email VARCHAR(50) NOT NULL,
-	phone VARCHAR(11) NOT NULL,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE accounts(
-	id INT NOT NULL AUTO_INCREMENT,
-	customer_id INT NOT NULL,
-	balance INT NOT NULL,
-    pass INT NOT NULL CHECK (pass BETWEEN 6 AND 10),
+CREATE TABLE accounts (
+	id int not null auto_increment,
+	customer_id int not null,
+	balance FLOAT,
 	PRIMARY KEY (id),
 	FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
 
-INSERT INTO customers (first_name, last_name, email, phone) VALUES
-	('James', 'Albright', 'james@atu.ie', '0831234567'),
-	('Bob', 'Dylan', 'Bob@atu.ie', '0831235678'),
-	('Kevin', 'Donnell', 'kevin@atu.ie', '0851234567');
-	
-INSERT INTO accounts (customer_id, pass, balance) VALUES
-	(1, 123456, 1200),
-	(2, 654321, 300),
-	(3, 123456789, 25);
-	
+INSERT INTO customers (name, userName, pass, email) VALUES
+	('James Albright', 'JamesA', 'secret', 'james@atu.ie'),
+	('Kevin Holland', 'KevinH', 'hush1', 'kevin@atu.ie'),
+	('Kerry Franklin', 'KerryF', 'neverguess', 'kerry@atu.ie'),
+	('John Bradley', 'JohnB', 'goodluck', 'john@atu.ie');
+
+INSERT INTO accounts (customer_id, balance) VALUES
+	(1, 2300),
+	(2, 5610),
+	(3, 1023.56),
+	(4, 23.65);
+
 COMMIT;
