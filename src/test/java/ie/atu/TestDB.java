2824181eba2130e55b37e8039c330cac3a686f70
James Albright <G00379271@atu.ie>
2023-04-21 21:45:34 +0100

    Added Junit testing & Fixed pom file
    Created Junit testing of the database where it checks if user is being made
    After testing it deletes the test account on both tables and resets the auto_increment value as to not mess with the order
    
    Weird issue with pom file as it was incorrectly trying to get a version of maven plugins, fixed by referring to working version

---
+++ src/test/java/ie/atu/TestDB.java
@@ -0,0 +1,63 @@
+package ie.atu;
+
+import ie.atu.bankapp.BankDB;
+import ie.atu.bankapp.BankDB_Connection;
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDB {
+    BankDB myBank = new BankDB();
+
+    @Test
+    public void checkUserAdded() {
+
+        //connect to db
+        try (Connection connection = BankDB_Connection.getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+
+                //fill in details for create method
+                String name = "Test McTester";
+                String userName = "TesterMcT";
+                String pass = "testing123";
+                String email = "test@atu.ie";
+
+                //create the customer
+                myBank.Create(name, userName, pass, email);
+
+                //check the db holds the expected values
+                int custID;
+
+                //order by desc id, checks newly added account
+                try (ResultSet rs = statement.executeQuery("SELECT * FROM customers ORDER BY id DESC;")){
+                    //point cursor to next entry
+                    rs.next();
+                    custID = rs.getInt("id");
+                    assertEquals(name, rs.getString("name"));
+                    assertEquals(userName, rs.getString("userName"));
+                    assertEquals(pass, rs.getString("pass"));
+                    assertEquals(email, rs.getString("email"));
+
+                }
+
+                //delete created account to keep db clean
+                myBank.Delete(custID);
+
+                //fix auto_increment value after testing
+                String alterCust = "ALTER TABLE customers AUTO_INCREMENT = " + custID + ";";
+                statement.executeUpdate(alterCust);
+                String alterAcc = "ALTER TABLE accounts AUTO_INCREMENT = " + custID + ";";
+                statement.executeUpdate(alterAcc);
+
+                connection.close();
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
