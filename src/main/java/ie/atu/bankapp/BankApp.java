82f66c2e2069846f1227d9f669804a8200e950ff
ciara <g00402357@atu.ie>
2023-04-24 20:22:05 +0100

    created another loop and added options for log in

--- src/main/java/ie/atu/bankapp/BankApp.java
+++ src/main/java/ie/atu/bankapp/BankApp.java
@@ -7,28 +7,53 @@ public class BankApp extends Bank{
     //face of the application
     //make it nice to interact with for the user
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        //all this to be cleaned up. everything in here should just be calling functions basically
 
-        System.out.println("Welcome to Banking App! ");
+        int a = -1;
+        int choice;
+        System.out.println("Welcome to the Banking App! "); //welcome page
 
+        System.out.println("Please make a choice : ");
         System.out.println("1. Log in");
         System.out.println("2. Create an Account");
         System.out.println("3. Exit");
+        choice = sc.nextInt();
 
-        int x = -1;
-        while(x != 1)
+
+
+        while(a != 1)
         {
-            switch (x){
+            switch (choice) //user has choice to og in or create account
+            {
                 case 1 : logIn();
                     break;
                 case 2: createAccount();
                 break;
-                case 3: x=1;
+                case 3: a = 1;
                 break;
             }
+        }
+        int logChoice;
+        while(choice == 1) // while choice is 1, user can check balance, withdraw and deposit
+        {
+            System.out.println("Please make a choice : ");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Deposit");
 
+            logChoice = sc.nextInt();
+
+                switch(logChoice)
+                {
+                    case 1 : checkBal();
+                    break;
+                    case 2 : withdrawal();
+                    break;
+                    case 3 : deposit();
+                    break;
+                }
         }
 
 
