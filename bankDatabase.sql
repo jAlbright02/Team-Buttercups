ecbc9ee1eca857986a85a024c87f903c34688439
jAlbright02 <114882053+jAlbright02@users.noreply.github.com>
2023-04-27 21:00:14 +0100

    Update bankDatabase.sql

--- bankDatabase.sql
+++ bankDatabase.sql
@@ -2,8 +2,6 @@ CREATE DATABASE bankDB;
 
 USE bankDB;
 
-BEGIN;
-
 CREATE TABLE customers (
 	id INT NOT NULL auto_increment,
 	name VARCHAR(50) NOT NULL,
@@ -21,6 +19,17 @@ CREATE TABLE accounts (
 	FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
 
+CREATE TABLE customer_info(
+	id INT NOT NULL AUTO_INCREMENT,
+	customer_id INT NOT NULL,
+	phone VARCHAR(10),
+    	street VARCHAR(30),
+    	city VARCHAR(25),
+    	county VARCHAR(15),
+	PRIMARY KEY (id),
+	FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
 INSERT INTO customers (name, userName, pass, email) VALUES
 	('James Albright', 'JamesA', 'secret', 'james@atu.ie'),
 	('Kevin Holland', 'KevinH', 'hush1', 'kevin@atu.ie'),
@@ -32,5 +41,10 @@ INSERT INTO accounts (customer_id, balance) VALUES
 	(2, 5610),
 	(3, 1023.56),
 	(4, 23.65);
+	
+INSERT INTO customer_info (customer_id, phone, street, city, county) VALUES
+	(1, "0833235767", "Gleann Noinin", "Galway", "Galway"),
+    	(2, "0833238467", "Fairgreen", "Balla", "Mayo"),
+    	(3, "0829485767", "Dyke Road", "Forster", "Roscommon"),
+	(4, "0829434689", "Ranelagh", "Dublin", "Dublin");
 
-COMMIT;
