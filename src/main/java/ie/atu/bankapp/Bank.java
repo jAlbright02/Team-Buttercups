929b2b260a96190ba2bdd9830ea4802176db02ce
ciara <g00402357@atu.ie>
2023-04-27 12:31:14 +0100

    fixed up issues

--- src/main/java/ie/atu/bankapp/Bank.java
+++ src/main/java/ie/atu/bankapp/Bank.java
@@ -2,38 +2,37 @@ package ie.atu.bankapp;
 import java.util.Scanner;
 public class Bank extends BankDB{
     private static int accNumber;
-
-    private static String userName;    //setup for the login
-    private static String name;
     private static String password;
     private static float balance;
     private static float transfer;
     private static String transferAccNum;
     private boolean config;
 
+
     //made Scanner object static, so it can be referenced in static methods
-    static Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
+    CustomerClass customer = new CustomerClass();
 
-
-        public static void logIn () {  //asking the user to enter their login details
+    public void logIn () {  //asking the user to enter their login details
             System.out.println("Enter account number");
             accNumber = sc.nextInt();
+            sc.nextLine();
             System.out.println("Enter name");
-            name = sc.next();
+            customer.setName(sc.nextLine());
             System.out.println("Enter password");
             password = sc.next();
         }
 
 
-        public static void deposit () { //depositing money to bank account
+        public void deposit () { //depositing money to bank account
             long amount;
             System.out.println("Amount to be deposited");
             amount = sc.nextLong();
             balance = balance + amount;
         }
 
-        public static void withdrawal() { //withdrawal
+        public void withdrawal() { //withdrawal
             float amount;
             System.out.println("Enter amount to be withdrawn");
             amount = sc.nextLong();
@@ -41,18 +40,18 @@ public class Bank extends BankDB{
             showBalance(accNumber);
         }
 
-    public static void transfer(){  //creating a transfer function
+    public void transfer(){  //creating a transfer function
         System.out.println("Please enter amount you desire to transfer");
         transfer = sc.nextLong();
         System.out.println("Please enter account number you wish to transfer funds to");
         String transferAccNum = sc.nextLine();
     }
 
-    public static void checkBal () {     //checking how much money the account holder has
+    public void checkBal () {     //checking how much money the account holder has
         System.out.println("Your balance is" +balance);
     }
 
-        public static void createAccount () {  //setup for new account
+        public void createAccount () {  //setup for new account
             System.out.println("Create new account: ");
             System.out.println("Enter first name: ");
             String firstName = sc.nextLine();
@@ -63,35 +62,13 @@ public class Bank extends BankDB{
             System.out.println("Create password: ");
             String pass = sc.nextLine();
             System.out.println("Enter email: ");
-            String email = sc.nextLine();
+            customer.setEmail(sc.nextLine());
 
             String fullName = firstName + " " + lastName;
 
-            Create(fullName, userName, pass, email);
+            Create(fullName, userName, pass, customer.getEmail());
 
             System.out.println("Account created!");
         }
 
-
-
-
-    //log in
-    //check user name
-    //check password
-    //take user input (Scanner)
-
-
-
-
-    //create account - the code for this is basically already made just hold it in a function
-    //take info - instead of a million print lines like i've left it as
-    //store into database - already done by my Create() function
-
-
-
-    //withdraw
-
-    //deposit
-
-    //delete acc (done, just make the menu and i'll fix the function to fit how its made)
 }
